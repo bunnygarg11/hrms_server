b22a21eea0f4c06bee78b75b3292be8fc25eb04b
Milan843 <milansrivastava99@gmail.com>
2019-12-05 16:37:51 +0530

    10

--- src/api/admin/admin_controllers/showemploye.js
+++ src/api/admin/admin_controllers/showemploye.js
@@ -1,11 +1,26 @@
 const User = require("../../users/user.model");
 const showEmploye = async (req, res) => {
-  const user = await User.find({ name: { $nin: "Admin" } })
+  const user = await User.find({ name: { $nin: ["CEO"] } })
     .select("name jobStatus")
-    .populate("designation_id department_id ", [
+    .populate("designation_id department_id prefix ", [
       "name"
     ]);
   console.log(user);
   res.send(user);
 };
 module.exports = { showEmploye };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/api/kraSheets/krasheetRouters.js
+++ src/api/kraSheets/krasheetRouters.js
@@ -3,6 +3,7 @@ const {check,validationResult}=require("express-validator")
 var router = express.Router();
 const adminauth = require("../middleware/adminauth");
 const userauth = require("../middleware/userauth")
+const common = require("../middleware/common")
 const managerauth = require("../middleware/managerauth")
 const { Addkra }=require("../kraSheets/kraSheetcontroller/addkraSheet")
 const { Viewkra }=require("../kraSheets/kraSheetcontroller/viewkrasheet")
@@ -11,8 +12,8 @@ const { updatekramanager }=require("../kraSheets/kraSheetcontroller/updateKraShe
 
 router.post("/user/addkra",userauth,Addkra);
 router.get("/user/viewkra/:year",userauth,Viewkra);
-router.get("/manager/viewkra",managerauth,viewkramanager);
-router.post("/manager/updatekra",managerauth,updatekramanager)
+router.get("/manager/viewkra",common,viewkramanager);
+router.post("/manager/updatekra",common,updatekramanager)
 
 
 module.exports = router;
